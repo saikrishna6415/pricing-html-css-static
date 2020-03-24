e71333d850a59d303461f0f98947aea54a477a7f
saikrishna <saikrishnakotagiri16@gmail.com>
2020-03-24 13:27:22 +0530

    modify switch button

--- assets/scripts/switch.js
+++ assets/scripts/switch.js
@@ -2,10 +2,10 @@ const switchAnnualMonthly = ()=>{
   if (slider.checked) {
       document.getElementById('basicPrice').innerHTML = `&dollar; 19.99`;
       document.getElementById('professionalPrice').innerHTML = `&dollar; 24.99`;
-      document.getElementById('masterPrice').innerHTML = `&dollar;39.99`;
+      document.getElementById('masterPrice').innerHTML = `&dollar; 39.99`;
     } else {
       document.getElementById('basicPrice').innerHTML = `&dollar; 199.99`;
-      document.getElementById('professionalPrice').innerHTML = `&dollar;249.99`;
+      document.getElementById('professionalPrice').innerHTML = `&dollar; 249.99`;
       document.getElementById('masterPrice').innerHTML =`&dollar; 399.99`;
     }    
 }
